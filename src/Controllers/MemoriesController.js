b8d18ac3a799d11e66245f85c973198bde6e0d22
tass2t <brunog90@gmail.com>
2022-07-14 21:10:35 +0200

    added file upload

--- src/Controllers/MemoriesController.js
+++ src/Controllers/MemoriesController.js
@@ -1,18 +1,26 @@
 import MemoryModel from "../Models/MemoryModel.js";
 
+import fs from "fs";
+
 export default {
   getMemories: async (req, res) => {
     const response = await MemoryModel.find();
 
     res.status(200).send({ memories: response });
   },
+
   createMemory: async (req, res) => {
     const newMemory = await new MemoryModel({
       ...req.body,
     });
 
+    newMemory.creationDate = new Date();
+    newMemory.nbLikes = 0;
+
     newMemory.save((err) => {
       if (err) return err;
     });
+
+    res.status(200).send({ msg: "fichier uploadé" });
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import express from "express";
 import MemoriesController from "./Controllers/MemoriesController.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images/");
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, Date.now() + "-" + Math.round(Math.random() * 1e9) + "." + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const mainRoute = express.Router();
 
 mainRoute.get("/memories", MemoriesController.getMemories);
-mainRoute.post("/memories", MemoriesController.createMemory);
+mainRoute.post(
+  "/memories",
+  upload.single("file"),
+  MemoriesController.createMemory
+);
 
 export default mainRoute;
